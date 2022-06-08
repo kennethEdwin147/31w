33a7600616ecc0cab61e3a4917f759663cff3a12
kennethEdwin147 <edwingbeti@gmail.com>
2022-06-07 23:21:07 -0400

    Continuation de la personnalisation stylistique de la page front-page.php

--- index.php
+++ index.php
@@ -14,3 +14,26 @@
 <?php endif; ?>
 
 <?php get_footer(); ?>
+
+
+
+<?php if( have_posts() ) : ?>
+    <section class='article_cours'>
+    <?php while( have_posts() ): the_post(); ?>
+      <article>
+        <header>
+          <p><?= the_title(); ?></p>
+        </header>
+        <p><?= wp_trim_words(get_the_content(), 30) ?></p>
+        <?php 
+         $lien = "<a href=' ". get_permalink(). " '>". substr(get_the_title(),0,8) ."</a>"; 
+         echo $lien;
+        ?>
+      </article>
+   <?php endwhile;?>
+   </section>
+<?php endif; ?>
+
+<?php get_footer(); ?>
+
+
