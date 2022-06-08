d96d672db50422ca094906268dfb8147d73ff298
kennethEdwin147 <edwingbeti@gmail.com>
2022-06-07 23:31:45 -0400

    Début de la personnalisation stylistique de la page single.php

--- index.php
+++ index.php
@@ -2,11 +2,11 @@
 <h2 class='article_cours_entete'>Explorez les cours de la technique en intégration multimédia:</h2>
 
 <?php if( have_posts() ) : ?>
-    <section class='article_cours'>
+    <section class='article_section'>
     <?php while( have_posts() ): the_post(); ?>
-      <article>
-        <header>
-          <p><?= the_title(); ?></p>
+      <article class='article_cours'>
+        <header class='article_cours__entete'>
+          <h3 class='article_cours__titre'><?= the_title(); ?></h3>
         </header>
       </article>
    <?php endwhile;?>
@@ -15,25 +15,5 @@
 
 <?php get_footer(); ?>
 
-
-
-<?php if( have_posts() ) : ?>
-    <section class='article_cours'>
-    <?php while( have_posts() ): the_post(); ?>
-      <article>
-        <header>
-          <p><?= the_title(); ?></p>
-        </header>
-        <p><?= wp_trim_words(get_the_content(), 30) ?></p>
-        <?php 
-         $lien = "<a href=' ". get_permalink(). " '>". substr(get_the_title(),0,8) ."</a>"; 
-         echo $lien;
-        ?>
-      </article>
-   <?php endwhile;?>
-   </section>
-<?php endif; ?>
-
-<?php get_footer(); ?>
 
 
--- single.php
+++ single.php
@@ -2,25 +2,26 @@
 <h2 class='article_cours_entete'>single.php</h2>
 
 <?php if( have_posts() ) : ?>
-    <section class='article_cours'>
+    <section class='article_section'>
     <?php while( have_posts() ): the_post(); ?>
-      <article>
-        <header>
-          <p><?= the_title(); ?></p>
-        </header>
-        <p><?= the_content() ?></p>
-        <?php 
-            $titre = get_the_title();
-            $sigle = substr($titre,0,8);
-            $heure = substr($titre ,-6);
-        ?>
-        <p>Sigle du cours: <?= $sigle ?></p>
-        <p>Nombre d'heures: <?= $heure ?></p>
+    <article class='article_cours'>
+      <header class='article_cours__entete'>
+        <h3 class='article_cours__titre'><?= the_title(); ?></h3>
+        <p class='article_cours__texte'><?= the_content() ?></p>
+      </header>
+      <?php 
+        $titre = get_the_title();
+        $sigle = substr($titre,0,8);
+        $heure = substr($titre ,-6);
+      ?>
+    <p class='article_cours__texte'>Sigle du cours: <?= $sigle ?></p>
+    <p class='article_cours__texte'>Nombre d'heures: <?= $heure ?></p>
+  </article>
 
-
-      </article>
    <?php endwhile;?>
    </section>
 <?php endif; ?>
 
 <?php get_footer(); ?>
+
+
