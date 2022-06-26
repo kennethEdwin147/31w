c2028883ebcc1942aae5c52a941c1ee20ba46964
kennethEdwin147 <edwingbeti@gmail.com>
2022-06-25 21:15:30 -0400

    Création du la front-page.php

---
+++ front-page.php
@@ -0,0 +1,18 @@
+<?php get_header(); ?>
+
+<header class='site__header'>
+  <h2>Front-page</h2>
+</header>
+
+<?php if( have_posts() ) : ?>
+    <?php while( have_posts() ) : the_post(); ?>
+          <h3><?= the_title(); ?></h3>
+          <p><?= wp_trim_words(get_the_content(), 25) ?></p>
+    <?php 
+        $lien = "<a href=' ". get_permalink(). " '>". substr(get_the_title(),0,8) ."</a>"; 
+        echo $lien;
+    ?>
+   <?php endwhile;?>
+<?php endif; ?>
+
+<?php get_footer(); ?>
