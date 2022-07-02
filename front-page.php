0b9cb3a8cb91788615796a91fc35aa18f4871453
kennethEdwin147 <edwingbeti@gmail.com>
2022-07-02 15:11:03 -0400

    semaine2-s1 modification du fichier front-page.php

--- front-page.php
+++ front-page.php
@@ -1,18 +1,27 @@
-<?php get_header(); ?> <!-- front-page.php -->
+<?php get_header(); ?>
 
-<header class='site__header'>
-  <h2>Bienvenue à la page d'accueil</h2> <!-- À enlever -->
-</header>
 
+<section class="site__main">
+        <section class="site__sidebar">
+            <img class="article_image" src="http://localhost:8080/31w-ken/wp-content/uploads/2022/07/FJ5f7ZHXMAAgzMh.jpg" alt="" srcset="">
+        </section>
+        
 <?php if( have_posts() ) : ?>
+    <section class='site__article'>
     <?php while( have_posts() ) : the_post(); ?>
-          <h3><?= the_title(); ?></h3>
-          <p><?= wp_trim_words(get_the_content(), 25) ?></p>
-    <?php 
-        $lien = "<a href=' ". get_permalink(). " '>". substr(get_the_title(),0,8) ."</a>"; 
-        echo $lien;
-    ?>
+      <article class="article_cours">
+          <h3 class='article_cours__titre'><?= the_title(); ?></h3>
+          <p class='article_cours__texte'><?= wp_trim_words(get_the_content(), 25) ?></p>
+        <?php 
+         $lien = "<a class='article_cours__bouton' href=' ". get_permalink(). " '>". substr(get_the_title(),0,8) ."</a>"; 
+         echo $lien;
+        ?>
+       </article>
+       <?php break; ?>
    <?php endwhile;?>
+   </section>
 <?php endif; ?>
+
+</section>
 
 <?php get_footer(); ?>
